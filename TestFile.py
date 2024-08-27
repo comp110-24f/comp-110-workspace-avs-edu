b52162b477b4d394856ddc60bbaf63dbb00837c0
avs-edu <sauce@unc.edu>
2024-08-27 16:48:18 -0400

    Finished Exercise 00

---
+++ TestFile.py
@@ -0,0 +1,8 @@
+def sum(num1: int, num2: int) -> int:
+  #Adds 2 numbers together
+  print(num1 + num2)
+
+sum(num1 = 30, num2 = 40)
+
+
+
---
+++ exercises/ex00_hello_world.py
@@ -0,0 +1,8 @@
+"""My First exercise in COMP110"""
+
+__author__ = "730769946"
+
+
+def greet(name: str) -> str:
+    # Welcoming first function definition
+    return "Hello, " + name + "!"
